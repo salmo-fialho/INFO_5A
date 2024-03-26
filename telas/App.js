b92383a5e25bddbc499693dc25ac810d2f44fc7c
salmo-fialho <samuelcarlof@gmail.com>
2024-03-26 12:26:42 -0400

    ALTERACOES

---
+++ telas/App.js
@@ -0,0 +1,14 @@
+//npm instal -g npm@10.5.0
+
+import React from "react";
+import {View, Text} from "react-native";
+
+function App (){
+    return(
+        <View>
+            <Text>Ola world</Text>
+        </View>
+    );
+}
+//dps tem de criar tenho que colocar o expor
+export default App; //poderia por antes da function
